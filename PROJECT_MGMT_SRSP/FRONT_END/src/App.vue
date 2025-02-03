6f3746fab33857ee3cea50531fcd1faa4f2ff0c4
zeehayat <zeenux@gmail.com>
2025-02-03 16:58:05 +0500

    TP

--- PROJECT_MGMT_SRSP/FRONT_END/src/App.vue
+++ PROJECT_MGMT_SRSP/FRONT_END/src/App.vue
@@ -1,27 +1,23 @@
-
 <template>
-  <div id="app">
-    <Menubar />
-    <router-view />
+  <div id="app" class="h-screen flex flex-col">
+    <Menubar v-if="isAuthenticated" />
+
+    <div class="flex-1 flex justify-center items-center bg-gray-100">
+      <router-view />
+    </div>
   </div>
-
 </template>
-
 
 <script>
 import Menubar from "./components/Menubar.vue";
+import { mapState } from "vuex";
 
 export default {
   components: {
     Menubar,
   },
+  computed: {
+    ...mapState(["isAuthenticated"]), // Maps isAuthenticated from store
+  },
 };
 </script>
-
-<style>
-/* Optional global styles */
-body {
-  margin: 0;
-  font-family: Arial, sans-serif;
-}
-</style>
--- PROJECT_MGMT_SRSP/FRONT_END/src/store/index.js
+++ PROJECT_MGMT_SRSP/FRONT_END/src/store/index.js
@@ -1,14 +1,23 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
-  },
-  getters: {
+    isAuthenticated: !!localStorage.getItem("accessToken"),
   },
   mutations: {
+    setAuthenticated(state, status) {
+      state.isAuthenticated = status;
+    },
   },
   actions: {
+    login({ commit }, token) {
+      localStorage.setItem("accessToken", token);
+      commit("setAuthenticated", true);
+    },
+    logout({ commit }) {
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("refreshToken");
+      commit("setAuthenticated", false);
+    },
   },
-  modules: {
-  }
-})
+});
